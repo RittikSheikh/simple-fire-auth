80f9653e1fedb1cb4d4bdc7778b32143361850d9
rittik sheikh <rittik11500@gamil.com>
2023-03-17 01:09:26 +0600

    all componets structure have been created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Lottie from 'lottie-react';
+import login from './assests/129750-login-orange.json'
+
+function App() {
+  return (
+    <div className="App">
+      <Lottie animationData={login}/>
+    </div>
+  );
+}
+
+export default App;
